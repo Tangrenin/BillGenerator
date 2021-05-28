11472ad16645c786780728d45434462e2841c65a
Tangrenin <quentindidier@msn.com>
2021-05-28 14:05:58 +0200

    WIP feat: started building gen_all_factures() functions

--- execution.py
+++ execution.py
@@ -1,36 +1,53 @@
 from pdf_builder import pdf_build
-from data_utils import data_extraction, available_months
+from data_utils import data_extraction, available_months, student_extraction
 
 
-def gen_all_factures(year, month):
+# TODO print students whose data have been unused for each month when processing
+
+def gen_all_factures(document_type, year, month):
     """
     generates all bills for a given month of a given year
     - fetches the appropriate data
     - puts together the variables that will be needed for building the pdf
-    - calls the pdf builder
+    - calls the pdf builder pdf_build with following args :
+    template_vars :
+    output_name :
+
+    :param document_type: -
     :param month: month about which bills should be generated
     :param year: year about which bills should be generated
     :return: nothing
     """
     # gets the data
+    students = student_extraction()
     data = data_extraction(year, month)
-    students = []
 
-    for student in students:
+    # TODO check on what exactly to iterate from the students dataframe
+    for index, student in students.iterrows():
+        # TODO check if a student name from the DB corresponds to a student in data, and match their data
+        # We look for the corresponding in the data sheet
+        for candidate in data:
+            prenom = candidate.split(' ')[0]
+            if len(candidate.split(' ')) > 1:
+                # If there's a surname, we recreate it if it contains multiple words
+                nom = candidate.split(' ', 1)
+
         # puts together the necessary variables
+        # TODO do what the last comments says
         output_name = ''
         template_vars = {}
 
         # calls the pdf builder
-        pdf_build(template_vars, 'facture', output_name)
+        pdf_build(template_vars, document_type, output_name)
 
 
-def gen_all_attest(year, month):
+def gen_all_attest(document_type, year, month):
     """
     generates all attestations for a given year
     - fetches the appropriate data
     - puts together the variables that will be needed for building the pdf
     - calls the pdf builder
+    :param document_type: -
     :param month: unused parameter
     :param year: year about which attestations should be generated
     :return: nothing
@@ -48,7 +65,7 @@ def gen_all_attest(year, month):
         template_vars = {}
 
         # calls the pdf builder
-        pdf_build(template_vars, 'attestation', output_name)
+        pdf_build(template_vars, document_type, output_name)
 
 
 def execute(document_type, year, month):
@@ -65,6 +82,6 @@ def execute(document_type, year, month):
     }
     if document_type == 'all':
         for func in gen_function:
-            gen_function[func](year, month)
+            gen_function[func](document_type, year, month)
     else:
-        gen_function[document_type](year, month)
+        gen_function[document_type](document_type, year, month)
