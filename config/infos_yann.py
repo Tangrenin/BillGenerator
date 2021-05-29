2b962306aa66dffee7751934f7fefec4c16f8be9
Tangrenin <quentindidier@msn.com>
2021-05-29 02:44:23 +0200

    feat: typed the info relative to teacher

---
+++ config/infos_yann.py
@@ -0,0 +1,11 @@
+from types_utils import Address
+
+firstname = "Yann"
+surname = "Chiffaudel"
+
+adresse = Address(street="16 rue Buffon",
+                  zipcode="91400",
+                  city="Orsay")
+
+siret = "881 443 576 00015"
+SAP = "SAP881443576"
