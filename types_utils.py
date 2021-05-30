5f1e4fb8434a82a918b87ac47cf1f8965c1dcdce
Tangrenin <quentindidier@msn.com>
2021-05-30 17:53:43 +0200

    feat: added custom errors to raise

--- types_utils.py
+++ types_utils.py
@@ -6,5 +6,23 @@ class Address:
     street: str
     zipcode: str
     city: str
-    country : str = "France"
+    country: str = "France"
     other: str = ""
+
+
+# Custom error classes
+
+class Error(Exception):
+    """Base class for other exceptions"""
+    pass
+
+
+class DuplicateStudentsError(Error):
+    """Raises when multiple students with the same name are found in a data sheet"""
+    def __init__(self, name):
+        self.name = name
+        self.message = f"\nL'élève {name} a été trouvé.e plusieurs fois dans le tableau d'heures de cours.\n" \
+                       f"Veuillez spécifier le nom des élèves de manière unique.\n" \
+                       f"Ex: Nina, Nina -> Nina B., Nina C.\n" \
+                       f"/!\\ Pensez à mettre à jour Infos_élèves en conséquence /!\\"
+        super().__init__(self.message)
